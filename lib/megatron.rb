c12cacc2c235963ad41f45eab6f595ab193b9911
Jerome Gravel-Niquet <jeromegn@gmail.com>
2015-08-07 12:43:50 -0400

    need to require the version

--- lib/megatron.rb
+++ lib/megatron.rb
@@ -2,6 +2,9 @@ Gem.loaded_specs['megatron'].dependencies.each do |d|
   require d.name
 end
 
+# Need access to the version
+require "megatron/version"
+
 require "megatron/form"
 require "megatron/engine"
 
