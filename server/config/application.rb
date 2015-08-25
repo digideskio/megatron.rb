99ec95455f0b1b39ecb8f58bcb6918a8e8f756fb
Brandon Mathis <brandon@imathis.com>
2015-08-25 11:29:56 -0400

    Added auto-prefixer to docs server

--- server/config/application.rb
+++ server/config/application.rb
@@ -4,6 +4,7 @@ require "rails"
 # Pick the frameworks you want:
 require "action_controller/railtie"
 require "action_view/railtie"
+require 'autoprefixer-rails'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
