feb0f2121479c2576c8c9b0ccc6b08c7b0673798
Brandon Mathis <brandon@imathis.com>
2015-10-01 14:31:07 -0500

    Finished up some final tweaks to navigation svg integration

--- app/helpers/megatron/application_helper.rb
+++ app/helpers/megatron/application_helper.rb
@@ -29,13 +29,17 @@ module Megatron
     end
 
     def text_icon(name, options={})
-      content_tag(:span, class: "text-icon") do
+      options[:class] ||= ''
+      options[:class] << " text-icon"
+      content_tag(:span, class: "text-icon-wrapper") do
         Esvg.svg_icon(name.to_s, options).html_safe
       end
     end
 
     def nav_icon(name, options={})
-      content_tag(:span, class: "text-icon nav-icon") do
+      options[:class] ||= ''
+      options[:class] << " text-icon"
+      content_tag(:span, class: "text-icon-wrapper nav-icon") do
         Esvg.svg_icon(name.to_s, options).html_safe
       end
     end
