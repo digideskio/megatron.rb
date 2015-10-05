971bd61d2d19389f8338323814a32c46de912041
Brandon Mathis <brandon@imathis.com>
2015-10-05 12:04:17 -0500

    text_icon helper now accepts size and color options

--- app/helpers/megatron/icon_helper.rb
+++ app/helpers/megatron/icon_helper.rb
@@ -20,8 +20,7 @@ module Megatron
     end
 
     def text_icon(name, options={})
-      options[:class] = default_class(options[:class], "text-icon")
-      options[:wrapper] = default_class(options[:wrapper], "icon-wrapper")
+      options = set_icon_classes(options, class: 'text-icon', wrapper: 'icon-wrapper')
 
       content_tag(:span, class: options[:wrapper]) do
         icon(name.to_s, options)
@@ -29,8 +28,27 @@ module Megatron
     end
 
     def nav_icon(name, options={})
-      options[:wrapper] = default_class(options[:wrapper], "nav-icon")
+      options[:wrapper] = (options[:wrapper] || '') << " nav-icon"
       text_icon(name, options)
+    end
+
+    def set_icon_classes(options, defaults={})
+      options[:class] || (options[:class] || '') << " #{defaults[:class]}"
+      options[:wrapper] = (options[:wrapper] || '') << " #{defaults[:wrapper]}"
+
+      # Automate adding color classes, blue becomes blue-text
+      # This should allow us to change it later more easily if necessary
+      if options[:color]
+        options[:class] << " #{options[:color]+='-text'}"
+      end
+
+      # Automate adding size classes, x-small becomes x-small-text
+      # This should allow us to change it later more easily if necessary
+      if options[:size]
+        options[:class] << " #{options[:size]+='-text'}"
+      end
+
+      options
     end
 
     def default_class(classnames, default)
